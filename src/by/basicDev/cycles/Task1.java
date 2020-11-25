a321a26974fd99d3778ba5b495daa98a95efd900
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2020-11-25 14:39:49 +0300

    add validation of input data

--- src/by/basicDev/cycles/Task1.java
+++ src/by/basicDev/cycles/Task1.java
@@ -8,7 +8,17 @@ import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int quantity = sc.nextInt();
+        int quantity;
+        do {
+            System.out.println("Please enter a positive number!");
+            while (!sc.hasNextInt()) {
+                System.out.println("That's not a number!");
+                sc.next();
+            }
+            quantity = sc.nextInt();
+        } while (quantity <= 0);
+        System.out.println("Thank you! Got " + quantity);
+
         int result = 0;
         for (int i = 0; i < quantity; i++) {
             result += i;
