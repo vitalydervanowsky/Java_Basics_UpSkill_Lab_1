f52160daa21fa80f7e93ab2c6307cef563d11ada
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2020-12-08 10:20:59 +0300

    add Task10 from Home Task: One-dimensional arrays

---
+++ src/by/algorithmization/oneDimArrays/Task10.java
@@ -0,0 +1,25 @@
+package by.algorithmization.oneDimArrays;
+
+public class Task10 {
+    public static void main(String[] args) {
+        int n = 8;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = (int) (20 * Math.random());
+            System.out.print("a[" + i + "]=" + a[i] + "\t");
+        }
+        System.out.println();
+        int[] b = new int[n];
+        int counter = 0;
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
+                b[i - counter] = a[i];
+            } else {
+                counter++;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.print("b[" + i + "]=" + b[i] + "\t");
+        }
+    }
+}
