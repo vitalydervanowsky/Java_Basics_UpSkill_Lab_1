c2801baffd9e70a544fd2caa8b8dabb9d0c2f086
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2020-12-08 12:27:15 +0300

    fix Task2 from Home Task: One-dimensional arrays * z is random now * array is filled random nums * delete global condition

--- src/by/algorithmization/oneDimArrays/Task2.java
+++ src/by/algorithmization/oneDimArrays/Task2.java
@@ -2,23 +2,20 @@ package by.algorithmization.oneDimArrays;
 
 public class Task2 {
     public static void main(String[] args) {
-        int z = 16;
         int counter = 0;
-        int start = 10;
-        int finish = 25;
-        int quantity = finish - start + 1;
-        int[] a = new int[quantity];
-        if (start <= z && z <= finish) {
-            for (int i = 0; i < quantity; i++) {
-                a[i] = i + start;
-                if (a[i] > z) {
-                    a[i] = z;
-                    counter++;
-                }
-                System.out.print(a[i] + "\t");
+        int start = 0;
+        int finish = 20;
+        int z = start + (int)((finish - start) * Math.random());
+        System.out.println(z);
+        int n = finish - start + 1;
+        int[] a = new int[n];
+        for (int i = start; i < finish; i++) {
+            a[i] = (int)((finish - start) * Math.random());
+            if (a[i] > z) {
+                a[i] = z;
+                counter++;
             }
-        } else {
-            System.out.print("Incorrect Z");
+            System.out.print(a[i] + "\t");
         }
         System.out.println();
         if (counter > 0) {
