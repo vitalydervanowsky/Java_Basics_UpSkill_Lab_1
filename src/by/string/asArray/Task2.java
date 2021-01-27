afb8ab8f620ed58c2ca9367065571fd3acb5cc06
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-01-27 12:36:14 +0300

    fix Task2 from Home Task:  Working with a string as an array of characters * use StringBuilder instead String * use method indexOf(String s, int fromIndex)

--- src/by/string/asArray/Task2.java
+++ src/by/string/asArray/Task2.java
@@ -1,5 +1,7 @@
 package by.string.asArray;
 
+// Замените в строке все вхождения 'word' на 'letter'.
+
 public class Task2 {
     public static void main(String[] args) {
         String str = "Дан массив word переменных в camelCase. Преобразовать word в snake_case. " +
@@ -7,14 +9,20 @@ public class Task2 {
         String find = "word";
         String replace = "letter";
         System.out.println(str);
-        while (str.contains(find)) {
-            int start = 0;
-            int finish = str.indexOf(find);
-            String temp = str.substring(start, finish) + replace;
+        int start = 0;
+        int finish;
+        StringBuilder b = new StringBuilder();
+        while (start < str.length()) {
+            finish = str.indexOf(find, start);
+            if (finish > 0) {
+                b.append(str, start, finish).append(replace);
+            } else {
+                finish = str.length();
+                b.append(str, start, finish);
+            }
             start = finish + find.length();
-            finish = str.length();
-            str = temp + str.substring(start, finish);
         }
+        str = b.toString();
         System.out.println(str);
     }
 }
