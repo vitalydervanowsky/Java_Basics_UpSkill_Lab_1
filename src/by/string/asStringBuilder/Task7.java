f8a354b6811310ccc04fe91c41bb500e44b01c11
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-01-29 13:15:19 +0300

    fix Task7 from Home Task: Working with a string as a String or StringBuilder * add method inputString() for working with Scanner * change condition

--- src/by/string/asStringBuilder/Task7.java
+++ src/by/string/asStringBuilder/Task7.java
@@ -7,26 +7,25 @@ import java.util.Scanner;
 
 public class Task7 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        String s = inputString();
+        StringBuilder b = new StringBuilder();
+        b.append(s.charAt(0));
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != b.charAt(b.length() - 1) && s.charAt(i) != ' ') {
+                b.append(s.charAt(i));
+            }
+        }
+        System.out.println("b=" + b);
+    }
+
+    static String inputString() {
         String s;
+        Scanner sc = new Scanner(System.in);
         do {
             System.out.println("Please enter a line!");
             s = sc.nextLine();
         } while (s.length() <= 0);
         System.out.println("s=" + s);
-        StringBuilder b = new StringBuilder();
-        for (int i = 0; i < s.length(); i++) {
-            boolean isExclusiveSymbol = true;
-            for (int j = 0; j < b.length(); j++) {
-                isExclusiveSymbol = s.charAt(i) != b.charAt(j);
-                if (!isExclusiveSymbol) {
-                    j = b.length();
-                }
-            }
-            if (isExclusiveSymbol && !(s.charAt(i) == " ".charAt(0))) {
-                b.append(s.charAt(i));
-            }
-        }
-        System.out.println("b=" + b);
+        return s;
     }
 }
