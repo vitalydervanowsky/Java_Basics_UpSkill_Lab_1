df3f8c7577b1d241d427a72604765b5f8f938211
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-01-29 13:11:24 +0300

    fix Task1 from Home Task: Working with a string as a String or StringBuilder * delete useless method isSame * change condition

--- src/by/string/asStringBuilder/Task1.java
+++ src/by/string/asStringBuilder/Task1.java
@@ -7,33 +7,19 @@ public class Task1 {
         String s = "       1.  Дан текст (строка). Найдите      наибольшее количество   подряд идущих пробелов в нем.\n" +
                 "2. В строке   вставить после каждого символа 'a'     символ 'b'.\n" +
                 "3. Проверить, является ли  заданное слово   палиндромом.";
-        String find = " ";
         System.out.println(s);
         int streak = 0;
         int maxStreak = streak;
-        boolean isStreak = false;
         for (int i = 0; i < s.length(); i++) {
-            if (isSame(s.charAt(i), find) && isStreak) {
+            if (Character.isWhitespace(s.charAt(i))) {
                 streak++;
                 if (streak > maxStreak) {
                     maxStreak = streak;
                 }
             } else {
-                if (isSame(s.charAt(i), find)) {
-                    isStreak = true;
-                    streak = 1;
-                } else {
-                    isStreak = false;
-                }
+                streak = 0;
             }
         }
         System.out.println(maxStreak);
-    }
-
-    static boolean isSame(char ch, String s) {
-        if (s != null && s.length() == 1) {
-            return s.charAt(0) == ch;
-        }
-        return false;
     }
 }
