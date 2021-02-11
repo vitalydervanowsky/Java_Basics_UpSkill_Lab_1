e457a099dcfa78b42ecd928b690e7ca2a1cb28b5
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-02-11 09:42:52 +0300

    add Task5 from Home Task: The simplest classes and objects

---
+++ src/by/classes/simplest/task5/Counter.java
@@ -0,0 +1,30 @@
+package by.classes.simplest.task5;
+
+// Опишите класс, реализующий десятичный счетчик, который может увеличивать или уменьшать свое значение на единицу
+// в заданном диапазоне. Предусмотрите инициализацию счетчика значениями по умолчанию и произвольными значениями.
+// Счетчик имеет методы увеличения и уменьшения состояния, и метод позволяющее получить его текущее состояние.
+// Написать код, демонстрирующий все возможности класса.
+
+public class Counter {
+    private int count;
+
+    public int getCount() {
+        return count;
+    }
+
+    public Counter() {
+        this.count = 0;
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public void plus() {
+        this.count++;
+    }
+
+    public void minus() {
+        this.count--;
+    }
+}
