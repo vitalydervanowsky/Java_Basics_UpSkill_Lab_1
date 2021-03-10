d1472fb5b575bc51f26faa03458bb532ce2bc11b
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-03-10 14:57:46 +0300

    fix Task2 from Home Task: Aggregation and composition * change constructor Car(String brand) -> Car(Engine e, Wheel w) * use method getBrand() instead var brand in methods of Car.class

--- src/by/classes/aggregation/task2/Car.java
+++ src/by/classes/aggregation/task2/Car.java
@@ -1,42 +1,39 @@
 package by.classes.aggregation.task2;
 
 public class Car {
-    private final String brand;
-    private final Engine engine = new Engine("DuratecHE", 1997, 145);
-    private Wheel wheel = new Wheel(205, 55, 16);
+    private String brand = "brand";
+    private final Engine engine;
+    private Wheel wheel;
 
-    public Car(String brand) {
-        this.brand = brand;
+    public Car(Engine engine, Wheel wheel) {
+        this.engine = engine;
+        this.wheel = wheel;
     }
 
     public void ride() {
-        System.out.println(brand + " rides on the road");
+        System.out.println(getBrand() + " rides on the road");
     }
 
     public void refuel() {
-        System.out.println(brand + " stopped at a gas station to refuel");
+        System.out.println(getBrand() + " stopped at a gas station to refuel");
     }
 
-    public void changeAWheel() {
-        System.out.print(brand + " stopped to change wheels from " + wheel.toString());
-        wheel.setWidth(wheel.getWidth() + 10);
-        wheel.setHeight(wheel.getHeight() - 10);
-        wheel.setDiameter(wheel.getDiameter() + 1);
-        System.out.println(" to " + wheel.toString());
-    }
-
-    public void changeAWheel(int width, int height, int diameter) {
-        System.out.print(brand + " stopped to change wheels from " + wheel.toString());
-        this.wheel = new Wheel(width, height, diameter);
-        System.out.println(" to " + wheel.toString());
+    public void changeAWheel(Wheel wheel) {
+        System.out.print(getBrand() + " stopped to change wheels from " + this.wheel.toString());
+        this.wheel = wheel;
+        System.out.println(" to " + this.wheel.toString());
     }
 
     public String getBrand() {
         return brand;
     }
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     @Override
     public String toString() {
-        return brand + " with engine " + engine.toString() + " on " + wheel.toString() + " wheels";
+        return getBrand() + " has an engine " + engine.toString() + " on " + wheel.toString() + " wheels";
     }
 }
--- src/by/classes/aggregation/task2/Main.java
+++ src/by/classes/aggregation/task2/Main.java
@@ -5,12 +5,15 @@ package by.classes.aggregation.task2;
 
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car("Ford");
+        Engine engine = new Engine("DuratecHE", 1997, 145);
+        Wheel wheel = new Wheel(205, 55, 16);
+        Car car = new Car(engine, wheel);
+        car.setBrand("Ford");
+        System.out.println("My new car is " + car.getBrand());
         car.ride();
         car.refuel();
-        car.changeAWheel();
-        car.changeAWheel(195, 65, 13);
-        System.out.println(car.getBrand());
+        Wheel newWheel = new Wheel(195, 65, 13);
+        car.changeAWheel(newWheel);
         System.out.println(car.toString());
     }
 }
