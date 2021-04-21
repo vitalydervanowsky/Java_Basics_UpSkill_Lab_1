9239bfe9564c0b3c04de6e6e8787084775c15d9d
Vitaly Dervanowsky <vitalydervanowsky@gmail.com>
2021-04-21 15:05:50 +0300

    add Task3 from Home Task: Basics of OOP

---
+++ src/by/oop/task3/Calendar.java
@@ -0,0 +1,15 @@
+package by.oop.task3;
+
+import java.util.ArrayList;
+
+public class Calendar {
+    private ArrayList<Day> days;
+
+    public ArrayList<Day> getDays() {
+        return days;
+    }
+
+    public void setDays(ArrayList<Day> days) {
+        this.days = days;
+    }
+}
---
+++ src/by/oop/task3/Day.java
@@ -0,0 +1,17 @@
+package by.oop.task3;
+
+public class Day {
+    private int weekDayNumber;
+
+    public Day(int weekDayNumber) {
+        this.weekDayNumber = weekDayNumber;
+    }
+
+    public int getWeekDayNumber() {
+        return weekDayNumber;
+    }
+
+    public void setWeekDayNumber(int weekDayNumber) {
+        this.weekDayNumber = weekDayNumber;
+    }
+}
